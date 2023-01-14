3cc4d507b43b41b10bc52c0ed9c0b82468ec3961
Fedor Chesnovski <72036206+chesnovski@users.noreply.github.com>
2023-01-14 22:16:44 +0100

    Update web.py

--- web.py
+++ web.py
@@ -98,4 +98,4 @@ def get_coin_info(symbol, exchange):
     d = f'{exchange}:{symbol}'
     return render_template('test.html', d = d)
 if __name__=='__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5000, host='0.0.0.0')
